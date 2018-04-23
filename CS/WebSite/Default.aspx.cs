e34fe0e82b1b13fc62bf7cae360f57d5202ba44e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxEditors;
+using DevExpress.Web.ASPxFormLayout;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) { }
+    protected void ASPxButton1_Click(object sender, EventArgs e) {
+        foreach(var item in layout.Items) {
+            SetValuesToActiveGroupTab(item as TabbedLayoutGroup);
+        }
+    }
+    private void SetValuesToActiveGroupTab(TabbedLayoutGroup group) {
+        if(group != null) {
+            LayoutGroupBase innerGroup = group.Items[group.PageControl.ActiveTabIndex] as LayoutGroupBase;
+            innerGroup.ForEach(IterateNestedControls);
+        }
+    }
+    void IterateNestedControls(LayoutItemBase item) {
+        foreach(LayoutItemBase c in item.Collection) {
+            if(c is LayoutGroup)
+                (c as LayoutGroup).ForEach(IterateNestedControls);
+            if(c is LayoutItem)
+                SetValue(c as LayoutItem);
+        }
+    }
+    static void SetValue(LayoutItem c) {
+        foreach(Control control in c.LayoutItemNestedControlContainer.Controls) {
+            ASPxEdit editor = control as ASPxEdit;
+            if(editor != null)
+                editor.Value = DateTime.Now.ToString();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASPxFormLayout - How to iterate through current tab items in TabbedLayoutGroup
+
+
+<p>This example demonstrates how to iterate through items on the current tab of the TabbedLayoutGroup. To get access to an active tab page, use the TabbedLayoutGroup.PageControl.ActiveTabIndex property.</p>
+
+<br/>
+
+
